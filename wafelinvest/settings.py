dfa11cd3e98bd743d07fd8db14f6803e539abf85
Admin <admin@Admins-MacBook-Pro.local>
2025-07-25 13:53:19 +0200

    Fix settings and WSGI references from wafelinvest to core

--- wafelinvest/settings.py
+++ wafelinvest/settings.py
@@ -33,7 +33,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'wafelinvest.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -51,7 +51,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'wafelinvest.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
--- wafelinvest/wsgi.py
+++ wafelinvest/wsgi.py
@@ -7,10 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
+"""
+ASGI config for wafelinvest project.
 
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wafelinvest.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 application = get_wsgi_application()
