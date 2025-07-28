b108885a13635ae49027705e41bb67b0f1778231
Admin <admin@Admins-MacBook-Pro.local>
2025-07-29 01:22:37 +0200

    Fix AppRegistryNotReady by correcting import order

--- wafelinvest/asgi.py
+++ wafelinvest/asgi.py
@@ -1,16 +1,19 @@
-# wafelinvest/asgi.py
-
 import os
 import django
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from core.routing import websocket_urlpatterns
 
+# Django ortamı başlatılmadan hiçbir şey import etme
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wafelinvest.settings')
 django.setup()
 
+# Bundan sonra importlar güvenli
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+from core.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
 })
